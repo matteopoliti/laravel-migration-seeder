93db4a093416f9a023203a8100733abf466986e2
matteopoliti <matteo.politi17@gmail.com>
2024-03-15 16:16:00 +0100

    Feat: add seeder

--- app/Http/Controllers/Folder/PageController.php
+++ app/Http/Controllers/Folder/PageController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers\Folder;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Train;
 
 class PageController extends Controller
 {
     public function index()
     {
-        return view('welcome');
+        $trains = Train::all();
+        return view('welcome', compact('trains'));
     }
 }
---
+++ database/seeders/TrainsTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Models\Train;
+
+class TrainsTableSeeder extends Seeder
+{
+    public function run(Faker $faker): void
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $newtrain = new Train();
+            $newtrain->company = $faker->company();
+            $newtrain->departure_station = $faker->city();
+            $newtrain->arrival_station = $faker->city();
+            $newtrain->departure_time = $faker->time();
+            $newtrain->arrival_time = $faker->time();
+            $newtrain->train_code = $faker->bothify('?#??##?#');
+            $newtrain->carriages = $faker->numberBetween(2, 15);
+            $newtrain->on_time = $faker->boolean();
+            $newtrain->cancelled = $faker->boolean();
+        }
+    }
+}
